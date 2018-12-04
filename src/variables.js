df89741f8d596884fe7155b5bc5d5a6906d6c062
utp <utp@utp>
2018-12-04 17:38:54 -0500

    Setting up Babel and introducing variables on ES6

---
+++ src/variables.js
@@ -0,0 +1,14 @@
+// We use let and const as a substitute of var, cause var can have problems with re declarations.
+const pi = 3.1416; //We use const for variables that we need to keep constant.
+let age = 23; //We use let for wariables that we need to change later.
+
+console.log("The number pi is " + pi);
+console.log("I am " + age + " years old.");
+
+//Also let and const use block scope. If I define a variable on any block with brackets (if, while, for, etc.) it cant be accessed from outside of that block.
+if (pi) {
+    const e = 4.30;
+    // pi = 3.1518;
+    // console.log(age);
+}   
+console.log(e);
