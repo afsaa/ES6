2956f703970964275655bdcd0f4e4de8810851de
UTP <utp@utp.local>
2018-12-05 14:54:27 -0500

    Object destructuring

---
+++ src/destructObjects.js
@@ -0,0 +1,12 @@
+// Object destructuring is really useful and is being used a lot by js developers and by popular frameworks like React, Angular and Vue.
+const song = {
+    title: 'One More Light',
+    band: 'Linkin Park',
+    genre: 'Alternative',
+    length: '3:10'
+};
+
+const { title, band, genre, length } = song;
+
+const getSongInfo = ( {title, band} ) => console.log(`The song ${title} was made by ${band}`);
+getSongInfo(song);
